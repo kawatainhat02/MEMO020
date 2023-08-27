91bcafffcd2d8080d73baebc56b89184feb52db7
Kawa Tai 119 <58183232+kawatainhat02@users.noreply.github.com>
2023-08-27 12:14:06 +0900

    Update Arduino.ino

--- Arduino.ino
+++ Arduino.ino
@@ -5,3 +5,17 @@ void loop() {
    Serial.println("Hello World!");
    delay(1000);
 }
+
+#define buzzerPin 7                // buzzer to arduino uno pin 7
+
+void setup(){
+   pinMode(buzzerPin, OUTPUT);    // Set buzzer - pin 9 as an output
+}
+
+void loop(){
+   tone(buzzerPin, 2000);     // Send 2000Hz sound signal...
+   delay(500);                // ...for 0.5 sec
+   noTone(buzzerPin);         // Stop sound...
+   delay(500);                // ...for 0.5 sec
+}
+
